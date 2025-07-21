5b112ebcff5a8909cff5724d960b0714c560d80f
Emmanuelle Kamwa <emmanuellekamwa@gmail.com>
2025-07-21 11:54:31 +0100

    Finished creating Gemini UI

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,6 +9,25 @@ const Main = () => {
                 <p>Gemini</p>
                 <img src={assets.user_icon} alt="" />
             </div>
+            <div className="main-container">
+                <div className="greet">
+                    <p><span>Hello, Emmanuelle.</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder='Ask Gemini'/>
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                        Gemini can send inaccurate responses, so please verify its responses before relying on them.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
